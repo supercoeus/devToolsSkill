fa98f15dfdc69644bb5559a9d25f95d8e5727d99
wy2008826 <760104178@qq.com>
2015-11-04 17:44:31 +0800

    add gulpfile

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -5,11 +5,11 @@ var gulpLoadPlugins=require("gulp-load-plugins");
 var plugins=gulpLoadPlugins();
 
 
-gulp.task("minify",function(){
-	gulp.src("src/js/*.js")
-	.pipe(plugins.uglify())
-	.pipe(gulp.dest("dis/js"))
-});
+// gulp.task("minify",function(){
+// 	gulp.src("src/js/*.js")
+// 	.pipe(plugins.uglify())
+// 	.pipe(gulp.dest("dis/js"))
+// });
 
 /****src**
 	1.支持多路径的数组集合
@@ -20,25 +20,47 @@ gulp.task("minify",function(){
 
 	//sass模块
 gulp.task("scss",function(){
-	gulp.src(["src/css/sass/**/*.scss","!src/css/sass/folder2/*.scss","src/css/sass_1/*.scss"])
+	gulp.src(["src/**/*.scss"])
 	.pipe(plugins.scss())
 	.pipe(gulp.dest("dis/css"));
 });
-	
-	//clean模块  删除文件和文件夹 ***删除文件和文件夹(文件夹有内容也会删除) **删除文件和空文件夹
-gulp.task("cleanAll",function(){
-	gulp.src(["dis/css/***","dis/js/*.js"])
-	.pipe(plugins.clean());
+
+gulp.task("scssA",function(){
+	gulp.src(["src/***.scss"])
+	.pipe(plugins.scss())
+	.pipe(gulp.dest("destA"));
 });
 
 
+gulp.task("scssB",function(){
+	gulp.src(["src/*.scss"])
+	.pipe(plugins.scss())
+	.pipe(gulp.dest("destB"));
+});
+
+
+gulp.task("scssNot",function(){
+	gulp.src(["src/**/*.scss","!src/a.scss"])
+	.pipe(plugins.scss())
+	.pipe(gulp.dest("destNot"));
+});
+
+
+
+	//clean模块  删除文件和文件夹 ***删除文件和文件夹(文件夹有内容也会删除) **删除文件和空文件夹
+// gulp.task("cleanAll",function(){
+// 	gulp.src(["src/css/***","drc/js/*.js"])
+// 	.pipe(plugins.clean());
+// });
+
+
 //任务的合并  后面的任务是异步执行的 不一定按照先后顺序完成
-gulp.task("all",["scss","minify"]);
+gulp.task("all",["scss","scssA","scssB","scssNot"]);
 
 
 //watch 和run方法
 gulp.task("watch",function(){
-	gulp.watch("src/css/sass/*.scss",function(){
+	gulp.watch("src/**/***.scss",function(){
 		gulp.run("scss");
 	});
 });
