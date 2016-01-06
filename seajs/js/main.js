253849b461df8124bf098c3160ec0faa544a835a
wy2008826 <760104178@qq.com>
2016-01-06 16:30:30 +0800

    change in index.html and main.js

--- seajs/js/main.js
+++ seajs/js/main.js
@@ -1,15 +1,27 @@
 define(function(require,exports,module){
 	var $=require("jquery");
+	var $1=require("jquery1");
 	var sayName=require("sayName");
 
 	console.log("require:"+require);
 	console.log("$:"+$);
+	console.log("$1"+$1);
 	console.log("sayName:"+sayName);
 
 	module.exports.a=function(){
 		console.log("a");
 	}
-	var body=$("body");
+	try{
+		var body1=$1("body");
+		console.log(body1);
+		var body=$("body");
+		console.log(body,body1);
+	}
+	catch(e){
+		console.log(e);
+	}
+	
+
 	sayName.say("习大大");
 	sayName.say("哈哈");
 
