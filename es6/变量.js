3d9876ced0a0f93d13557cf8841d5f8880c92692
wy2008826 <760104178@qq.com>
2016-03-22 16:00:32 +0800

    add es6

--- es6/变量.js
+++ es6/变量.js
@@ -29,11 +29,52 @@ console.log(b);//直接报错
 var a=1;
 let b=2;
 
-//暂时性死区
+//暂时性死区 最好的使用习惯是先声明   后使用
 
 var tmp=123;
 if(true){
 	tmp=244;
 	let tmp;
-	console.log(tmp);//直接报错   代码块内，使用let命令声明变量之前，该变量是不可用的
+	console.log(tmp);//直接报错   代码块内，使用let命令声明变量之前，该变量是不可用的 
 }
+
+
+
+//let不允许在同一个区块内同时声明同一个变量
+
+if(true){//报错
+	var a=1;
+	let a=1;
+}
+
+
+function(a){//报错
+	let a=1;
+}
+
+function(a){//不报错
+	if(true){
+		let a=2;
+	}
+}
+
+
+function test(){
+	let a="testOutside Let";
+	function inner(){
+		console.log(a);
+		a="12312321";
+		console.log(a);
+	}
+	inner();
+}
+test();//函数内部声明的let可以被该函数内的其他内部函数调用得到
+
+
+
+if(true){
+	function a(){
+		console.log(213213);
+	}
+}
+a();
