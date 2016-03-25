5c7a78fa3e2ef56d948cf90b9c2e0bd193387f1b
wy2008826 <760104178@qq.com>
2016-03-25 13:59:18 +0800

    add things in promise.js

--- es6/promise.js
+++ es6/promise.js
@@ -7,15 +7,50 @@
 //只能一级级联  怎么实现多级异步调用呢？
 //一个一步操作的结果可能是一个值也可能是一个promise实例，如果是一个值的话，promise就终止了then的级联调用
 
-function timeout(ms) {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, ms,"step1");
-  });
+
+//Promise.prototype.then()
+//then方法的第一个参数resolved状态的回调函数 第二个参数是rejected状态的回调函数
+
+
+
+// function timeout(ms) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(resolve, ms,"step1");
+//   });
+// }
+
+// timeout(2000).then((value) => {//2s之后才会去执行then方法后面的回调
+//   console.log(value);
+// });
+
+
+
+
+//Promise.prototype.catch()
+//catch用于指定发生错误时候的回调函数 
+var promise=new Promise(function(resolve,reject){
+ 	resolve("aaa1");
+ 	resolve("aaa2");
+ 	
+});
+
+
+var promiseSomeThingSync=function(val){
+	return new Promise(function(resolve,reject){
+		resolve(val);
+	});
 }
 
-timeout(2000).then((value) => {//2s之后才会去执行then方法后面的回调
-  console.log(value);
-});
+promiseSomeThingSync("aaa1").then(function(val){
+	console.log(val);
+	return promiseSomeThingSync("aaa2");
+}).then(function(val){
+	console.log(val);
+	return promiseSomeThingSync("aaa3");
+}).then(function(val){
+	console.log(val);
+	return promiseSomeThingSync("aaa4");
+})
 
 
 console.log("outer");
@@ -25,14 +60,9 @@ console.log("outer");
 
 
 
-
-
-
-
-
 //一个小技巧  settimeout的第三个参数开始传递的是延时函数的对应参数  所以不需要bind对应的参数了
-setTimeout(test,1000,"test1","test2");
-function test(val1,val2){
-  console.log(val1,val2);
-}
+// setTimeout(test,1000,"test1","test2");
+// function test(val1,val2){
+//   console.log(val1,val2);
+// }
 
