3853b14c952b3734e1db1d100c2b615e3c0be14a
wy2008826 <760104178@qq.com>
2016-03-27 11:17:35 +0800

    add

--- es6/generator.js
+++ es6/generator.js
@@ -14,3 +14,34 @@ console.log(test.next());//{value:"hello",done:false}
 console.log(test.next());//{value:"world",done:false}
 console.log(test.next());//{value:"success",done:true}
 
+
+
+
+function animate1(val,time){
+	setTimeout(function(){
+		console.log(val);
+	},time);
+}
+function animate2(val,time){
+	setTimeout(function(){
+		console.log(val);
+	},time);
+}
+function animate3(val,time){
+	setTimeout(function(){
+		console.log(val);
+	},time);
+}
+
+
+
+function* testPromise(){
+	yield animate1("step1",1000);
+	yield animate2("step2",2000);
+	yield animate3("step3",3000);
+}
+
+var geneRator=testPromise();
+console.log(geneRator.next());//{value:"hello",done:false}
+console.log(geneRator.next());//{value:"world",done:false}
+console.log(geneRator.next());//{value:"success",done:true}
