aec19739124a5658f0cfd8e28534f12ff7a0d40d
wy2008826 <760104178@qq.com>
2016-11-10 16:14:37 +0800

    add

--- es6/generator.js
+++ es6/generator.js
@@ -2,21 +2,86 @@
 var fetch=require("node-fetch");
 
 
-//generator函数会返回一个遍历器 每次执行遍历器会返回函数内部的一个状态
+/*
+
+generator函数不能解决异步调用的序列化问题，而是用于返回一个状态机 其实提供了一种可以暂停执行的函数
+
+调用Generator函数后，该函数并不执行，返回的也不是函数运行结果，而是一个指向内部状态的指针对象
+
+yield后面可以跟随数据 以及执行函数   不能直接跟随函数  直接跟随函数相当于注册了一个局部函数  但并不会执行
+
+遇到yield语句，就暂停执行后面的操作，并将紧跟在yield后面的那个表达式的值 如果直接跟随一个函数，没有任何实际意义
+
+yield和return的区别 
+
+
+问题：
+
+通过next 可以向内部传递参数么？
+
+*/
+
+
 function* hellowGenerator(){
 	yield "hello";
-	yield "world";
+	yield "wor"+"ld";
+	
+
+	yield function(){
+		console.log("a run function");
+	}();
+
+	yield function(){
+		console.log("function");
+		return "function return"
+	}();
+
+	yield function aaa(){
+		console.log("not run function");
+		return "function return"
+	};
+
+	aaa();//函数运行至此报错 因为aaa并没有定义 上面的yield语句后面的函数不会执行  也不会挂载到当前的局部环境中
 
 	return "success";
 }
 
 var test=hellowGenerator();
 
+
 console.log(test.next());//{value:"hello",done:false}
 console.log(test.next());//{value:"world",done:false}
+console.log(test.next());//{done:false} 
+console.log(test.next());//{value:undefined,done:false} 同时有console打印
+console.log(test.next());//{value:"function return",done:false}  同时有打印
 console.log(test.next());//{value:"success",done:true}
 
+/*
+通过循环执行
+for(key of test){
+	console.log(key);
+}
 
+
+var arr = [1, [[2, 3], 4], [5, 6]];
+
+var flat = function* (a) {
+  var length = a.length;
+  for (var i = 0; i < length; i++) {
+    var item = a[i];
+    if (typeof item !== 'number') {
+      yield* flat(item);
+    } else {
+      yield item;
+    }
+  }
+};
+
+for (var f of flat(arr)) {
+  console.log(f);
+}
+
+*/
 function* fetchData(){
 	var _t=new Date()*1;
 	var url="http://m.maizuo.com/v4/api/film/now-playing?_t="+_t+"&page=1&count=10000";
@@ -27,9 +92,35 @@ function* fetchData(){
 
 var gData=fetchData();
 var result=gData.next();
+/*
 result.value.then(function(data){
 	console.log(data);
 }).then(function(data){
 	gData.next(data);
 });
+
+*/
+
+/*
+
+下面的timeout函数不能用于流程控制
+如果一个yield执行了异步调用  下一个yield并不知道上一个异步调用是否完成，所以无法解决回调地狱问题
+
+*/
+
+
+function *timeout(){
+	yield setTimeout(function(){
+		console.log(1);
+	},1000);
+
+	yield setTimeout(function(){
+		console.log(2);
+	},500);
+
+}
+
+var timeGen=timeout();
+timeGen.next();
+timeGen.next();
 
