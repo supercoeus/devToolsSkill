53e564c593eefbcfdb528a5882828e403157cfe6
wy2008826 <760104178@qq.com>
2016-11-29 15:00:01 +0800

    add

--- es6/async.js
+++ es6/async.js
@@ -47,4 +47,17 @@ let [foo, bar] = await Promise.all([getFoo(), getBar()]);
 
 */
 
+async function logInOrder(urls) {
+  // 并发读取远程URL
+  const textPromises = urls.map(async url => {
+    const response = await fetch(url);
+    return response.text();
+  });
+
+  // 按次序输出
+  for (const textPromise of textPromises) {
+    console.log(await textPromise);
+  }
+}
+
 
